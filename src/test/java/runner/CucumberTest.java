fde0675f794536c8802bac6e601842b622a6b40d
gitJayash <jayashsandaruwan@gmail.com>
2025-09-21 23:47:51 +0530

    add locator constants with this commit

--- src/test/java/runner/CucumberTest.java
+++ src/test/java/runner/CucumberTest.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/resources/features/test_project1/module1/LoginDemo.feature", glue = {"StepDefinitions"},
+@CucumberOptions(features = "src/resources/features/test_project1/module1", glue = {"StepDefinitions"},
 monochrome = true,
         plugin = {"pretty", "html:target/HTMLReports"}
 )
